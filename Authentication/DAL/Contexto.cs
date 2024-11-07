6c676efb950f825654140253ef71cfa9981dacc9
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-07 16:38:38 -0400

    Add project files.

---
+++ Authentication/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Authentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<UserAccount> UserAccounts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UserAccount>().HasData(new List<UserAccount>()
+        {
+            new UserAccount () { Id = 1, UserName = "Alaina", Password = "Admin01", Role = "Admin" },
+            new UserAccount () { Id = 2, UserName = "Juan", Password = "User02", Role = "User" }
+        });
+    }
+}
---
+++ Authentication/Models/ViewModels/LoginViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authentication.Models.ViewModels;
+
+public class LoginViewModels
+{
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor ingrese nombre de usuario")]
+    public string? UserName { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor ingrese su password")]
+    public string? Password { get; set; }
+}
